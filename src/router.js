f01b4a520ac6e84201de88867c922d5d206e2b3e
maatcha <mattcha54@gmail.com>
2020-04-17 17:59:47 +0200

    gsap intro

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Modal from './views/Modal.vue'
 import Drawer from './views/Drawer.vue'
 import Cards from './views/Cards.vue'
+import Simple from './views/Simple.vue'
+import Stagger from './views/Stagger.vue'
 
 Vue.use(Router)
 
@@ -24,6 +26,16 @@ export default new Router({
       path: '/cards',
       name: 'cards',
       component: Cards
+    },
+    {
+      path: '/stagger',
+      name: 'stagger',
+      component: Stagger
+    },
+    {
+      path: '/simple',
+      name: 'simple',
+      component: Simple
     },
     {
       path: '/list',
---
+++ src/views/Stagger.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="container">
+    <div v-for="card in cards" :key="card.id" class="card"></div>
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+export default {
+  data() {
+    return {
+      cards: [
+        { id: 1231 },
+        { id: 1232 },
+        { id: 1233 },
+        { id: 1234 },
+        { id: 1235 },
+        { id: 1236 }
+      ]
+    }
+  },
+  mounted() {
+    gsap.from('.card', {
+      duration: 0.5,
+      opacity: 0,
+      scale: 0,
+      y: 200,
+      ease: 'power1',
+      stagger: {
+        each: 0.1,
+        // from: 'edges'
+        from: 'center'
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+.card {
+  height: 6.5em;
+  width: 6.5em;
+  border-radius: 1%;
+  background-color: #16c0b0;
+  box-shadow: 0.08em 0.03em 0.4em #ababab;
+  padding-top: 1em;
+  margin-top: 0.5em;
+  margin-right: 0.5em;
+}
+</style>
